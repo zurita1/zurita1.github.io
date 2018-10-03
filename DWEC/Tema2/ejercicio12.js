b2a05f90cdff4bb8326f89509ce780ce75f26a14
Rafa García Zurita <zurita1rafa@gmail.com>
2018-10-03 09:47:36 +0200

    corrigiendo

---
+++ DWEC/Tema2/ejercicio12.js
@@ -0,0 +1,13 @@
+{
+    window.addEventListener("load",init);
+
+    function muestra() {
+        document.getElementById("texto").className = "visible";  
+    document.getElementById("mensaje").className = "oculto";
+        
+    }
+
+    function init() {
+        document.getElementById("mensaje").addEventListener("click", muestra);
+    }
+}
