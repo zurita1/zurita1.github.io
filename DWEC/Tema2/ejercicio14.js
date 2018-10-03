14b12fe00c1bf369cd0795dcf3f25f2cfee98f95
Zurita <zurita1rafa@gmail.com>
2018-10-03 19:45:06 +0200

    subiendo ejer

---
+++ DWEC/Tema2/ejercicio14.js
@@ -0,0 +1,20 @@
+{
+    window.addEventListener("load",init);
+
+    function muestraOculta(id) {
+        var elemento = document.getElementById('contenidos_'+id);
+        var enlace = document.getElementById('enlace_'+id);
+        
+        if(elemento.style.display == "" || elemento.style.display == "block") {
+            elemento.style.display = "none";
+            enlace.innerHTML = 'Mostrar contenidos';
+        }
+        else {
+            elemento.style.display = "block";
+            enlace.innerHTML = 'Ocultar contenidos';
+        }
+    }
+    function init(){
+        document.getElementById('todo').addEventListener("click",muestraOculta);
+    }
+}
