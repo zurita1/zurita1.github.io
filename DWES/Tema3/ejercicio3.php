81af8f38de2dafe6ef21baf25461b9b97e760ce8
Rafa García Zurita <zurita1rafa@gmail.com>
2018-10-05 09:50:01 +0200

    cor

--- DWES/Tema3/ejercicio3.php
+++ DWES/Tema3/ejercicio3.php
@@ -4,8 +4,12 @@
 * @package       Ejericicios Basicos
 * @author        Rafa García Zurita
 */
-   for($i = 1; $i < 10; $i++){
-      echo $i . '<br />';
-   }
+for ($tabla=1; $tabla<=10 ; $tabla++) { 
+    echo "<h2> Tabla del " . $tabla . "</h2>";
+
+    for ($i=1; $i <= 10; $i++) { 
+        echo $tabla . " x " . $i . " = " . ($tabla*$i) . "</br>";
+    }
+}
    
 ?>
