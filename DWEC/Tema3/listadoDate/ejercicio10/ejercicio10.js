5fffc94aa7585a8bec4c47380947d8e1c844b484
Rafa García Zurita <zurita1rafa@gmail.com>
2018-11-07 08:41:41 +0100

    cor

--- DWEC/Tema3/listadoDate/ejercicio10/ejercicio10.js
+++ DWEC/Tema3/listadoDate/ejercicio10/ejercicio10.js
@@ -1,14 +1,16 @@
-{  
+{
     function init() {
-        info=document.getElementById("info");
-        info.innerHTML=calcularHastaFinDeAnno();
+        document.getElementById("info").innerHTML = calcularHastaFinDeAnno();
     }
-    function calcularHastaFinDeAnno(){
+    function calcularHastaFinDeAnno() {
         let annioActual = new Date().getFullYear();
+        console.log(annioActual);
         let fechaActual = new Date().getTime();
-        let finDeAnnio = new Date(annioActual,11,31).getTime();
+        console.log(fechaActual);
+        let finDeAnnio = new Date(annioActual, 11, 31).getTime();
+        console.log(finDeAnnio);
         let resul = finDeAnnio - fechaActual;
-        return Math.round(resul/(1000*60*60*24));
+        return Math.round(resul / (1000 * 60 * 60 * 24));
     }
-window.addEventListener("load", init);
+    window.addEventListener("load", init);
 }
