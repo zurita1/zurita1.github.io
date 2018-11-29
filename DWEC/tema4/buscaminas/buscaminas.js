bebd68405d3dc6b882d373409b2a4a27387c5b37
Zurita <zurita1rafa@gmail.com>
2018-11-29 17:09:52 +0100

    Buscaminas

--- DWEC/tema4/buscaminas/buscaminas.js
+++ DWEC/tema4/buscaminas/buscaminas.js
@@ -6,13 +6,41 @@
     let tablero = [];
     let x;
     let y;
+    let valor;
+    let body;
+    let input;
 
     function init() {
+        body = document.getElementsByTagName('body')[0];
         nivel = parseInt(prompt("Elige tu dificultad del 1 al 3 (si es menos de 1, se asignara este y si es mayor de tres se asignara este)"));
         elegirNivel();
         crearTablero()
         añadirMinas();
-        console.table(tablero);
+        console.log(tablero);
+        crearTableroVisual();
+       
+    }
+    function crearTableroVisual() {
+        contenedor = document.createElement('div');
+        body.appendChild(contenedor);       
+        for (let i = 0; i < filas; i++) {
+            for (let j = 0; j < columnas; j++) {
+                input=document.createElement('input');               
+                input.type = 'button';
+                input.value = "";
+                input.valor= tablero[i][j];
+                input.style.width = 50 +"px";
+                input.style.height = 50+ 'px';               
+                input.addEventListener("click", () => funcionalidad(i, j));
+                tablero[i][j]=input;
+                contenedor.appendChild(input);
+            }
+           saltoLinea=document.createElement("br");
+            contenedor.appendChild(saltoLinea);
+        }
+    }
+    function funcionalidad(i,j){
+           tablero[i][j].value=tablero[i][j].valor;
     }
     function crearTablero() {
         for (let i = 0; i < filas; i++) {
@@ -34,19 +62,19 @@
         }
         else {
             minas = 99;
-            columnas = 16;
+            columnas = 25;
             filas = 30;
         }
     }
     function añadirMinas() {
         for (let i = 0; i < minas; i++) {
-            x = Math.floor(Math.random() * filas);
-            y = Math.floor(Math.random() * columnas);
+            x = Math.floor(Math.random()*filas);
+            y = Math.floor(Math.random()*columnas);
             tablero[x][y] = 9;
             for (let j = Math.max(x - 1, 0); j <= Math.min(x + 1, filas - 1); j++)
                 for (let k = Math.max(y - 1, 0); k <= Math.min(y + 1, columnas - 1); k++)
                     if (tablero[j][k] !== 9)
-                        tablero[j][k] += 1;
+                        tablero[j][k]+=1;
         }
     }
     window.addEventListener("load", init)
