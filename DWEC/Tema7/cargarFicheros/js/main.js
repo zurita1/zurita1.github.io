03a0650a488736c5c3e7e34103514cae210548e1
Zurita <zurita1rafa@gmail.com>
2019-03-06 19:55:42 +0100

    Cargar ficheros corregidos

--- DWEC/Tema7/cargarFicheros/js/main.js
+++ DWEC/Tema7/cargarFicheros/js/main.js
@@ -1,27 +1,29 @@
 
 function init() {
     let mensaje;
+    let estados=  $("#estados");
     $("#cargar").click(function () {
-        ruta = $("#ruta").val();
+        let ruta = $("#ruta").val();
         $.get({
+            method: "GET",
             url:ruta,
             cache: "false",
             error: function () {
                 mensaje = "No Inicializada";
-                $("#estados").html(mensaje);
+                estados.html(mensaje);
             },
             success: function (data) {
                 mensaje += ", Éxito"
-                $("#estados").html(mensaje);
-                $("#fichero").html(data);
+                estados.html(mensaje);
+                $("#fichero").text(data);
             },
             complete: function () {
                 mensaje += ", Completada"
-                $("#estados").html(mensaje);
+                estados.html(mensaje);
             },
             beforeSend: function () {
                 mensaje = "Antes de enviar";
-                $("#estados").html(mensaje);
+                estados.html(mensaje);
             }
         });
 
