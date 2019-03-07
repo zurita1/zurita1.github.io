65dd376058e9405e8f08cc3ec605bf804759ee72
Zurita <zurita1rafa@gmail.com>
2019-03-07 20:50:50 +0100

    Subida plugin

--- DWEC/Tema6/plugin/js/jquery.js
+++ DWEC/Tema6/plugin/js/jquery.js
@@ -1,35 +1,41 @@
 (function ($) {
-    jQuery.fn.examen = function () {
-        let inputError = [];
+    let validator = {
+        nombre: [/^[a-zA-Zñúíóáé]{3,}([ ][a-zA-Zñúíóáé]{3,}){0,20}$/, "Mínimo 3 caracteres para el nombre."],
+        apellidos: [/^[a-zA-Zñúíóáé]{3,}([ ][a-zA-Zñúíóáé]{3,}){0,20}$/, "Mínimo 3 caracteres para el apellido."],
+        email: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/, "Correo inválido. Ejemplo:examen@exm.c"],
+        textarea: [/a/, "El contenido no puede estar vacío."],
+    
 
-        let validator = {
-            nombre: [/^[a-zA-Zñúíóáé]{3,}([ ][a-zA-Zñúíóáé]{3,}){0,20}$/, "Mínimo 3 caracteres para el nombre."],
-            apellidos: [/^[a-zA-Zñúíóáé]{3,}([ ][a-zA-Zñúíóáé]{3,}){0,20}$/, "Mínimo 3 caracteres para el apellido."],
-            email: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/, "Correo inválido. Ejemplo:examen@exm.c"],
-            textarea: [/a/, "El contenido no puede estar vacío."]
-        }
-        function validar(valor, expresion) {
+    }
+    let tester={
+        validar(valor, expresion) {
             if (!validator[expresion][0].test(valor))
                 return false;
             return true;
         }
-        $.extend({
-           
-        });
+    }
+    $.fn.examen = function (estilos2) {
+        let estilos = {
+          color: "black",
+          "background-color": "#A9F5A9",
+          border: "2px solid #0B6121"
+        };
+    
+        $.extend(estilos, estilos2);
+               
+        let inputError = [];
         $("input[type=text]").blur(function (event) {
             event.preventDefault();
-
+            
             let tipo = $(this).attr("tipo");
             let valor = $(this).val();
-            if (validar(valor, tipo)) {
-                $(this).css({
-                    color: "green",
-                    border: "1px solid green"
-                });
+            if (tester.validar(valor, tipo)) {
+                $(this).css(estilos);
             } else {
                 $(this).css({
                     color: "red",
-                    border: "1px solid red"
+                    border: "1px solid red",
+                    "background-color": "#FA5858",
                 });
                 inputError.push($(this));
             }
@@ -37,20 +43,19 @@
 
         $("input[type=text], textarea").focus(function (event) {
             event.preventDefault();
+           
             $(this).css({
                 color: "black",
-                border: "2px solid #ffD3D7"
+                border: "2px solid "
             });
         });
 
         $("input[type=submit]").click(function (event) {
-
-            console.log("entra")
             event.preventDefault();
             let $inputs = $("input[type=text], textarea");
+            inputError = [];
             $inputs.blur();
             if (inputError.length > 0) {
-                console.log("if")
                 inputError[0].focus();
             } else {
                 console.log("else")
@@ -63,7 +68,7 @@
             }
         
         });
-
+        return this;
 
 }  
 }) (jQuery);
